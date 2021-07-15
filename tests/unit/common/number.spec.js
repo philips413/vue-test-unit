27419b39b5f0aca4d1b2a048d6b95d74c0e5ac1e
KimDoRyeong <53990342+philips413@users.noreply.github.com>
2021-07-16 02:48:00 +0900

    Update number.spec.js

--- tests/unit/common/number.spec.js
+++ tests/unit/common/number.spec.js
@@ -2,7 +2,14 @@ import {NumberUtil} from "../../../src/components/common/NumberUtil.js";
 
 describe("공통 컴포넌트 테스트", () => {
     it("백단위 구분자 삽입되는가?", () => {
+        // given
         const num = 1000+"";
-        expect(NumberUtil.commas(num)).toBe("1,000");
+        cosnt goal = "1,000";
+        
+        // when
+        let result = NumberUtil.commas(num);
+        
+        // then
+        expect(result).toBe(goal);
     })
 });
